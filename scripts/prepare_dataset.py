5a87b70dbcaaea931f29822e0c5b308e32aade40
Joseph Bethge <Jopyth@users.noreply.github.com>
2017-09-12 17:34:18 +0200

    rewrite to only write one numpy array

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -19,84 +19,84 @@ def assert_debug(boolean, info):
     print info
     assert False
 
-def export(root, output, subset, current_file, candidates, target_voxel_mm, cubesize, progress = 0, loading_bar = None, preview = False):
-    scan, origin, spacing = helper.load_itk(os.path.join(root, subset, current_file + ".mhd"))
+def export_subset(args, subset, candidates):
+    files = os.listdir(os.path.join(args.root, subset))
+    files = [i.replace(".mhd","") for i in filter(lambda x: ".mhd" in x, files)]
+    files.sort()
 
-    scan = helper.rescale_patient_images(scan, spacing, target_voxel_mm)
-    spacing = np.asarray([target_voxel_mm, target_voxel_mm, target_voxel_mm])
-    scan = helper.normalize_to_grayscale(scan).astype(helper.DTYPE)
+    total = sum([len(candidates[f]) for f in files])
+    print "Exporting %s with %d candidates..." % (subset, total)
+    lb = helper.SimpleLoadingBar("Exporting", total)
 
-    total_candidates = len(candidates)
+    for f in files:
 
-    data = np.zeros((total_candidates, cubesize, cubesize, cubesize), dtype = helper.DTYPE)
-    labels = np.zeros((total_candidates), dtype = helper.DTYPE)
+        scan, origin, spacing = helper.load_itk(os.path.join(root, subset, current_file + ".mhd"))
 
-    data_filename = os.path.join(subset, "%s_data.npy" % current_file)
-    labels_filename = os.path.join(subset, "%s_labels.npy" % current_file)
+        scan = helper.rescale_patient_images(scan, spacing, target_voxel_mm)
+        spacing = np.asarray([target_voxel_mm, target_voxel_mm, target_voxel_mm])
+        scan = helper.normalize_to_grayscale(scan).astype(helper.DTYPE)
 
-    cubesize_arr = np.asarray((cubesize, cubesize, cubesize))
-    half_cubesize_arr = cubesize_arr / 2
+        total_candidates = len(candidates)
 
-    directory = os.path.join(output, subset)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+        data = np.zeros((total_candidates, cubesize, cubesize, cubesize), dtype = helper.DTYPE)
+        labels = np.zeros((total_candidates), dtype = helper.DTYPE)
 
-    candidate_num = 0
+        data_filename = os.path.join(subset, "%s_data.npy" % current_file)
+        labels_filename = os.path.join(subset, "%s_labels.npy" % current_file)
 
-    with open(os.path.join(output, subset + ".txt"), "a") as handle:
-        handle.write(" ".join([data_filename, labels_filename]) + "\n")
+        cubesize_arr = np.asarray((cubesize, cubesize, cubesize))
+        half_cubesize_arr = cubesize_arr / 2
 
-    for c in candidates:
+        directory = os.path.join(output, subset)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-        candidate_coords = np.asarray((float(c['coordZ']), float(c['coordY']), float(c['coordX'])))
-        voxel_coords = np.round(helper.world_to_voxel(candidate_coords, origin, spacing))
+        candidate_num = 0
 
-        z0, y0, x0 = sanitize_coords(voxel_coords - half_cubesize_arr, 0)
-        z1, y1, x1 = sanitize_coords(voxel_coords + half_cubesize_arr, 0)
+        with open(os.path.join(output, subset + ".txt"), "a") as handle:
+            handle.write(" ".join([data_filename, labels_filename]) + "\n")
 
-        candidate_roi = scan[z0:z1, y0:y1, x0:x1]
+        for c in candidates:
 
-        coordinates = [z0, y0, x0, z1, y1, x1]
-        assert_debug(min(candidate_roi.shape) > 0, coordinates)
-        assert_debug(max(candidate_roi.shape) <= cubesize, coordinates)
+            candidate_coords = np.asarray((float(c['coordZ']), float(c['coordY']), float(c['coordX'])))
+            voxel_coords = np.round(helper.world_to_voxel(candidate_coords, origin, spacing))
 
-        padding = [(int(math.ceil(p / 2.0)), int(p / 2.0)) for p in (cubesize_arr - candidate_roi.shape)]
-        padded = np.pad(candidate_roi, padding, 'constant', constant_values = (0,))
+            z0, y0, x0 = sanitize_coords(voxel_coords - half_cubesize_arr, 0)
+            z1, y1, x1 = sanitize_coords(voxel_coords + half_cubesize_arr, 0)
 
-        if preview: show_preview(padded, np.asarray((0., 0., 0.)), np.asarray((1., 1., 1.)))
+            candidate_roi = scan[z0:z1, y0:y1, x0:x1]
 
-        data[candidate_num,:,:,:] = padded
-        labels[candidate_num] = int(c['class'])
+            coordinates = [z0, y0, x0, z1, y1, x1]
+            assert_debug(min(candidate_roi.shape) > 0, coordinates)
+            assert_debug(max(candidate_roi.shape) <= cubesize, coordinates)
 
-        candidate_num += 1
-        if loading_bar:
-            loading_bar.update_progress(progress + candidate_num)
+            padding = [(int(math.ceil(p / 2.0)), int(p / 2.0)) for p in (cubesize_arr - candidate_roi.shape)]
+            padded = np.pad(candidate_roi, padding, 'constant', constant_values = (0,))
 
-    np.save(os.path.join(output, data_filename), data)
-    np.save(os.path.join(output, labels_filename), labels)
+            if preview: show_preview(padded, np.asarray((0., 0., 0.)), np.asarray((1., 1., 1.)))
 
-    return total_candidates
+            data[candidate_num,:,:,:] = padded
+            labels[candidate_num] = int(c['class'])
+
+            candidate_num += 1
+            if loading_bar:
+                loading_bar.update_progress(progress + candidate_num)
+
+        np.save(os.path.join(output, data_filename), data)
+        np.save(os.path.join(output, labels_filename), labels)
 
 def main(args):
     subsets = ["subset" + str(i) for i in args.subsets]
 
     candidates = helper.load_candidates(args.root)
 
-    total = sum([len(candidates[c]) for c in candidates])
-    print "Exporting %d candidates..." % total
-    lb = helper.SimpleLoadingBar("Exporting", total)
-
     for subset in subsets:
         with open(os.path.join(args.output, subset + ".txt"), "w") as handle:
             handle.write("")
 
-    progress = 0
+    # todo: augmentation, flip (3D?), resize ratio 0.8 - 1.15, rotate 90 degree
     for subset in subsets:
-        files = os.listdir(os.path.join(args.root, subset))
-        files = [i.replace(".mhd","") for i in filter(lambda x: ".mhd" in x, files)]
-        files.sort()
-        for current_file in files:
-            progress += export(args.root, args.output, subset, current_file, candidates[current_file], args.voxelsize, args.cubesize, progress, lb, args.preview)
+        export_subset(args, subset, candidates)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "prepare dataset for FPRED")
