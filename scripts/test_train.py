628ed461fe11cb281d94df8ed112f8f7ccd8947f
Joseph Bethge <Jopyth@users.noreply.github.com>
2017-09-14 09:46:07 +0200

    fix simple net

--- scripts/test_train.py
+++ scripts/test_train.py
@@ -18,15 +18,15 @@ def main(args):
     data = mx.sym.flatten(data=data)
 
     # The first fully-connected layer and the corresponding activation function
-    fc1  = mx.sym.FullyConnected(data=data, num_hidden=128)
+    fc1  = mx.sym.FullyConnected(data=data, num_hidden=256)
     act1 = mx.sym.Activation(data=fc1, act_type="relu")
 
     # The second fully-connected layer and the corresponding activation function
-    fc2  = mx.sym.FullyConnected(data=act1, num_hidden = 64)
+    fc2  = mx.sym.FullyConnected(data=act1, num_hidden = 128)
     act2 = mx.sym.Activation(data=fc2, act_type="relu")
 
     # MNIST has 10 classes
-    fc3  = mx.sym.FullyConnected(data=act2, num_hidden=10)
+    fc3  = mx.sym.FullyConnected(data=act2, num_hidden=2)
     # Softmax with cross entropy loss
     mlp  = mx.sym.SoftmaxOutput(data=fc3, name='softmax')
 
@@ -37,9 +37,9 @@ def main(args):
     mlp_model.fit(train_iter,  # train data
             eval_data=val_iter,  # validation data
             optimizer='sgd',  # use SGD to train
-            optimizer_params={'learning_rate':0.1},  # use fixed learning rate
+            optimizer_params={'learning_rate': 0.1},  # use fixed learning rate
             eval_metric='acc',  # report accuracy during training
-            batch_end_callback = mx.callback.Speedometer(batch_size, 100), # output progress for each 100 data batches
+            batch_end_callback = mx.callback.Speedometer(batch_size, 20), # output progress for each 100 data batches
             num_epoch=10)  # train for at most 10 dataset passes
 
     test_iter = CandidateIter(args.root, test_subsets, batch_size = batch_size)
