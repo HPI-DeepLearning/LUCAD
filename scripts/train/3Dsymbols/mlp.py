515081b40f6d9ae4878ae9c8fbaf0b645101f1d7
haojin.yang <semamediadata@gmail.com>
2017-09-14 16:45:46 +0200

    changed training control file

--- scripts/train/3Dsymbols/mlp.py
+++ scripts/train/3Dsymbols/mlp.py
@@ -11,14 +11,15 @@ def get_symbol(num_classes=2, **kwargs):
     data = mx.symbol.Variable('data')
 
     #test 3D conv
+    '''
     conv1 = mx.symbol.Convolution(
-        data=data, kernel=(5, 5, 5), stride=(2, 2, 2), num_filter=96)
+        data=data, kernel=(5, 5, 5), stride=(2, 2, 2), num_filter=96, layout="CDHW")
     bn1 = mx.sym.BatchNorm(data=conv1, fix_gamma=fix_gamma, eps=eps, momentum=bn_mom)
     relu1 = mx.symbol.Activation(data=bn1, act_type="relu")
     pool1 = mx.symbol.Pooling(
         data=relu1, pool_type="max", kernel=(3, 3, 3), stride=(2,2,2))
-
-    data = mx.sym.Flatten(data=pool1)
+    '''
+    data = mx.sym.Flatten(data=data)
     fc1  = mx.symbol.FullyConnected(data = data, name='fc1', num_hidden=128)
     act1 = mx.symbol.Activation(data = fc1, name='relu1', act_type="relu")
     fc2  = mx.symbol.FullyConnected(data = act1, name = 'fc2', num_hidden = 64)
--- scripts/train/train_luna.py
+++ scripts/train/train_luna.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     data.add_data_args(parser)
     data.add_data_aug_args(parser)
     # WE SHOULD BUILD AUG funcion in 3D
-    #data.set_data_aug_level(parser, 3)
+    data.set_data_aug_level(parser, 3)
 
     parser.add_argument('--pretrained', type=str,
                     help='the pre-trained model')
