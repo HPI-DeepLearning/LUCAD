3689126f259286168fb21f2309db4effe2fa0c85
haojin.yang <semamediadata@gmail.com>
2017-09-15 13:51:42 +0200

    adapted resnet for 3D conv

--- scripts/train/3Dsymbols/resnet.py
+++ scripts/train/3Dsymbols/resnet.py
@@ -31,20 +31,20 @@ def residual_unit(data, num_filter, stride, dim_match, name, bottle_neck=True, b
         # the same as https://github.com/facebook/fb.resnet.torch#notes, a bit difference with origin paper
         bn1 = mx.sym.BatchNorm(data=data, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_bn1')
         act1 = mx.sym.Activation(data=bn1, act_type='relu', name=name + '_relu1')
-        conv1 = mx.sym.Convolution(data=act1, num_filter=int(num_filter*0.25), kernel=(1,1), stride=(1,1), pad=(0,0),
+        conv1 = mx.sym.Convolution(data=act1, num_filter=int(num_filter*0.25), kernel=(1,1,1), stride=(1,1,1), pad=(0,0,0),
                                    no_bias=True, workspace=workspace, name=name + '_conv1')
         bn2 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_bn2')
         act2 = mx.sym.Activation(data=bn2, act_type='relu', name=name + '_relu2')
-        conv2 = mx.sym.Convolution(data=act2, num_filter=int(num_filter*0.25), kernel=(3,3), stride=stride, pad=(1,1),
+        conv2 = mx.sym.Convolution(data=act2, num_filter=int(num_filter*0.25), kernel=(3,3,3), stride=stride, pad=(1,1,1),
                                    no_bias=True, workspace=workspace, name=name + '_conv2')
         bn3 = mx.sym.BatchNorm(data=conv2, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_bn3')
         act3 = mx.sym.Activation(data=bn3, act_type='relu', name=name + '_relu3')
-        conv3 = mx.sym.Convolution(data=act3, num_filter=num_filter, kernel=(1,1), stride=(1,1), pad=(0,0), no_bias=True,
+        conv3 = mx.sym.Convolution(data=act3, num_filter=num_filter, kernel=(1,1,1), stride=(1,1,1), pad=(0,0,0), no_bias=True,
                                    workspace=workspace, name=name + '_conv3')
         if dim_match:
             shortcut = data
         else:
-            shortcut = mx.sym.Convolution(data=act1, num_filter=num_filter, kernel=(1,1), stride=stride, no_bias=True,
+            shortcut = mx.sym.Convolution(data=act1, num_filter=num_filter, kernel=(1,1,1), stride=stride, no_bias=True,
                                             workspace=workspace, name=name+'_sc')
         if memonger:
             shortcut._set_attr(mirror_stage='True')
@@ -52,16 +52,16 @@ def residual_unit(data, num_filter, stride, dim_match, name, bottle_neck=True, b
     else:
         bn1 = mx.sym.BatchNorm(data=data, fix_gamma=False, momentum=bn_mom, eps=2e-5, name=name + '_bn1')
         act1 = mx.sym.Activation(data=bn1, act_type='relu', name=name + '_relu1')
-        conv1 = mx.sym.Convolution(data=act1, num_filter=num_filter, kernel=(3,3), stride=stride, pad=(1,1),
+        conv1 = mx.sym.Convolution(data=act1, num_filter=num_filter, kernel=(3,3,3), stride=stride, pad=(1,1,1),
                                       no_bias=True, workspace=workspace, name=name + '_conv1')
         bn2 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, momentum=bn_mom, eps=2e-5, name=name + '_bn2')
         act2 = mx.sym.Activation(data=bn2, act_type='relu', name=name + '_relu2')
-        conv2 = mx.sym.Convolution(data=act2, num_filter=num_filter, kernel=(3,3), stride=(1,1), pad=(1,1),
+        conv2 = mx.sym.Convolution(data=act2, num_filter=num_filter, kernel=(3,3,3), stride=(1,1,1), pad=(1,1,1),
                                       no_bias=True, workspace=workspace, name=name + '_conv2')
         if dim_match:
             shortcut = data
         else:
-            shortcut = mx.sym.Convolution(data=act1, num_filter=num_filter, kernel=(1,1), stride=stride, no_bias=True,
+            shortcut = mx.sym.Convolution(data=act1, num_filter=num_filter, kernel=(1,1,1), stride=stride, no_bias=True,
                                             workspace=workspace, name=name+'_sc')
         if memonger:
             shortcut._set_attr(mirror_stage='True')
@@ -88,28 +88,32 @@ def resnet(units, num_stages, filter_list, num_classes, image_shape, bottle_neck
     assert(num_unit == num_stages)
     data = mx.sym.Variable(name='data')
     data = mx.sym.BatchNorm(data=data, fix_gamma=True, eps=2e-5, momentum=bn_mom, name='bn_data')
-    (nchannel, height, width) = image_shape
+    (nchannel, depth, height, width) = image_shape
     if height <= 32:            # such as cifar10
-        body = mx.sym.Convolution(data=data, num_filter=filter_list[0], kernel=(3, 3), stride=(1,1), pad=(1, 1),
+        body = mx.sym.Convolution(data=data, num_filter=filter_list[0], kernel=(3, 3, 3), stride=(1,1,1), pad=(1, 1, 1),
+                                  no_bias=True, name="conv0", workspace=workspace)
+    elif height == 36:
+        body = mx.sym.Convolution(data=data, num_filter=filter_list[0], kernel=(5, 5, 5), stride=(1,1,1),
                                   no_bias=True, name="conv0", workspace=workspace)
     else:                       # often expected to be 224 such as imagenet
-        body = mx.sym.Convolution(data=data, num_filter=filter_list[0], kernel=(7, 7), stride=(2,2), pad=(3, 3),
+        body = mx.sym.Convolution(data=data, num_filter=filter_list[0], kernel=(7, 7, 7), stride=(2,2,2), pad=(3, 3, 3),
                                   no_bias=True, name="conv0", workspace=workspace)
         body = mx.sym.BatchNorm(data=body, fix_gamma=False, eps=2e-5, momentum=bn_mom, name='bn0')
         body = mx.sym.Activation(data=body, act_type='relu', name='relu0')
-        body = mx.symbol.Pooling(data=body, kernel=(3, 3), stride=(2,2), pad=(1,1), pool_type='max')
-
+        body = mx.symbol.Pooling(data=body, kernel=(3, 3, 3), stride=(2,2,2), pad=(1,1,1), pool_type='max')    
     for i in range(num_stages):
-        body = residual_unit(body, filter_list[i+1], (1 if i==0 else 2, 1 if i==0 else 2), False,
+        body = residual_unit(body, filter_list[i+1], (1 if i==0 else 2, 1 if i==0 else 2, 1 if i==0 else 2), False,
                              name='stage%d_unit%d' % (i + 1, 1), bottle_neck=bottle_neck, workspace=workspace,
-                             memonger=memonger)
+                             memonger=memonger)    
         for j in range(units[i]-1):
-            body = residual_unit(body, filter_list[i+1], (1,1), True, name='stage%d_unit%d' % (i + 1, j + 2),
-                                 bottle_neck=bottle_neck, workspace=workspace, memonger=memonger)
+            body = residual_unit(body, filter_list[i+1], (1,1,1), True, name='stage%d_unit%d' % (i + 1, j + 2),
+                                 bottle_neck=bottle_neck, workspace=workspace, memonger=memonger)    
+    
     bn1 = mx.sym.BatchNorm(data=body, fix_gamma=False, eps=2e-5, momentum=bn_mom, name='bn1')
     relu1 = mx.sym.Activation(data=bn1, act_type='relu', name='relu1')
     # Although kernel is not used here when global_pool=True, we should put one
-    pool1 = mx.symbol.Pooling(data=relu1, global_pool=True, kernel=(7, 7), pool_type='avg', name='pool1')
+    # this kernel should be adapted for proper input size, the original kernel here is (7, 7)
+    pool1 = mx.symbol.Pooling(data=relu1, global_pool=True, kernel=(4, 4, 4), pool_type='avg', name='pool1')
     flat = mx.symbol.Flatten(data=pool1)
     fc1 = mx.symbol.FullyConnected(data=flat, num_hidden=num_classes, name='fc1')
     return mx.symbol.SoftmaxOutput(data=fc1, name='softmax')
@@ -120,7 +124,7 @@ def get_symbol(num_classes, num_layers, image_shape, conv_workspace=256, **kwarg
     Original author Wei Wu
     """
     image_shape = [int(l) for l in image_shape.split(',')]
-    (nchannel, height, width) = image_shape
+    (nchannel, depth, height, width) = image_shape
     if height <= 28:
         num_stages = 3
         if (num_layers-2) % 9 == 0 and num_layers >= 164:
