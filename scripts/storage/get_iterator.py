36c7c7d9d36ebfd87244178cc4ded8269eace783
Joseph Bethge <Jopyth@users.noreply.github.com>
2017-09-18 12:20:37 +0200

    add convenience function to get correct iterator for given dataset

---
+++ scripts/storage/get_iterator.py
@@ -0,0 +1,13 @@
+from util import helper
+
+from candidate_iterator import CandidateIter
+from distributed_iterator import DistributedIter
+
+
+def get_iterator(root, subsets, batch_size, chunk_size = 100, shuffle = False, prefetch = False):
+    info = helper.read_info_file(os.path.join(root, "subset%d" % subsets[0], "info.txt"))
+    if "type" not in info or info["type"] == "CandidateIter":
+        return CandidateIter(root, subsets, batch_size = batch_size, shuffle = shuffle, chunk_size = chunk_size)
+    if info["type"] == "DistributedIter":
+        return DistributedIter(root, subsets, batch_size = batch_size, prefetch = prefetch, shuffle = shuffle)
+    return None
