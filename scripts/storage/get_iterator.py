97cd373685ab3f4862ff8f9aeb639e85276308fa
Joseph Bethge <Jopyth@users.noreply.github.com>
2017-09-18 13:30:13 +0200

    add to get iterator

--- scripts/storage/get_iterator.py
+++ scripts/storage/get_iterator.py
@@ -5,10 +5,10 @@ from candidate_iterator import CandidateIter
 from distributed_iterator import DistributedIter
 
 
-def get_iterator(root, subsets, batch_size, chunk_size = 100, shuffle = False, prefetch = False):
+def get_iterator(root, subsets, batch_size, chunk_size = 100, shuffle = False, prefetch = False, data_name = 'data', label_name = 'softmax_label'):
     info = helper.read_info_file(os.path.join(root, "subset%d" % subsets[0], "info.txt"))
     if "type" not in info or info["type"] == "CandidateStorage":
-        return CandidateIter(root, subsets, batch_size = batch_size, shuffle = shuffle, chunk_size = chunk_size)
+        return CandidateIter(root, subsets, batch_size = batch_size, shuffle = shuffle, chunk_size = chunk_size, data_name = data_name, label_name = label_name)
     if info["type"] == "DistributedStorage":
-        return DistributedIter(root, subsets, batch_size = batch_size, prefetch = prefetch, shuffle = shuffle)
+        return DistributedIter(root, subsets, batch_size = batch_size, prefetch = prefetch, shuffle = shuffle, data_name = data_name, label_name = label_name)
     return None
