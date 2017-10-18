ab969e46007e53a529b9832bed0ed766e95645e9
Joseph Bethge <Jopyth@users.noreply.github.com>
2017-10-18 17:10:12 +0200

    nice graph

--- scripts/scoring/make_graphs.R
+++ scripts/scoring/make_graphs.R
@@ -4,7 +4,6 @@ data <- read.csv("concat.csv")
 data$class <- as.factor(data$class)
 
 ggplot(data, aes(x=probability, fill=class)) +
-  geom_histogram(binwidth=0.05) +
-  facet_grid(class ~ ., scales="free_y") +
-  scale_y_log10()
-
+  stat_bin(binwidth=0.05, position="dodge") +
+  stat_bin(binwidth=0.05, geom="text", aes(label=..count..), vjust=-0.5, size=3, position="dodge") + 
+  coord_cartesian(ylim=c(0, 1500))
